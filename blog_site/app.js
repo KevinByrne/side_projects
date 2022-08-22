dd96fa26be8c8a3b3d0a52cc6268ca3983ea3d23
KevinByrne <kevbee79@gmail.com>
2022-08-22 12:00:15 +0100

    updated to blog site

---
+++ blog_site/app.js
@@ -0,0 +1,4 @@
+const express = require('express'); /* retrieves express library installed earlier via 'npm i express' */
+const app =  express() /* calls express as a function */
+
+app.listen(3000) /* assigns listen function to app variable and passes port number as argument */
