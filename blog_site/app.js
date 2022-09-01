1f13cb1ca1b9d26930e0de0abd920a89cc52f946
KevinByrne <kevbee79@gmail.com>
2022-09-01 14:02:09 +0100

    24:41

--- blog_site/app.js
+++ blog_site/app.js
@@ -1,28 +1,28 @@
-const express = require('express'); /* retrieves express library installed earlier via 'npm i express' */
-const app =  express() /* calls express as a function */
-const mongoose = require('mongoose') /* mongoose library */
+const express = require('express') 
 const articleRouter = require('./routes/articles')
+const app = express()
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/blog') /* connects to defined db */
+mongoose.connect('mongodb://localhost/blog')
+
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
 
-app.use(express.urlencoded({extended: false}))
-
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>{
   const articles = [{
     title: 'test article',
     createdAt: new Date(),
     description: 'Test description'
   },
-{
-  title: 'test article 2',
-  createdAt: new Date(),
-  description: 'Test description 2'
-}]
-  res.render('articles/index', { articles: articles })
-})  
+  {
+    title: 'test article2',
+    createdAt: new Date(),
+    description: 'Test description2'
+  }]
 
-app.use('/articles', articleRouter)
-app.listen(3000) /* assigns listen function to app variable and passes port number as argument */ 
+  res.render('articles/index', { articles: articles })
+})
+
+app.listen(3000)
